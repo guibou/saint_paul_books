390d4fa421ff29ccfb132e1a0e276ae739f0323f
Guillaume Bouchard <guillaum.bouchard@gmail.com>
2025-03-31 08:48:49 +0400

    fix: correctly wraps `refreshBooks` with exception handling
    All the liftIO things *MUST* be wrapped with `try`, because otherwise it
    risks crashing the eventloop.

--- app/ApiXmlXhr.hs
+++ app/ApiXmlXhr.hs
@@ -25,25 +25,23 @@ refreshBook userEvent = performEventAsync $ refreshBooksCallback <$> userEvent
 refreshBooksCallback :: (MonadIO m) => User -> (Either Text (UTCTime, [Book]) -> IO ()) -> m ()
 refreshBooksCallback credential callback = do
   void $ liftIO $ async $ do
-    resM <- refreshBooks credential
-    callback resM
+    resM <- try $ refreshBooks  credential
+    res <- case resM of
+      Right res -> pure res
+      Left (err :: SomeException) -> do
+        pure $ Left (Text.pack $ show err)
+
+    callback res
   pure ()
 
 refreshBooks :: User -> IO (Either Text (UTCTime, [Book]))
 refreshBooks User {..} = do
-  resM <- try $ do
-    session <- getIguanaSession
-    auth <- getLogin session credential
-    items <- getLoan auth
-    -- TODO: handle error here
-    pure items
-
-  case resM of
-    Right res -> do
-      case eitherDecode @[JSONBook] (encode res) of
-        Right books -> do
-          mtime <- getCurrentTime
-          pure $ Right (mtime, coerce books)
-        Left err -> pure (Left $ Text.pack err)
-    Left (err :: SomeException) -> do
-      pure $ Left (Text.pack $ show err)
+  session <- getIguanaSession
+  auth <- getLogin session credential
+  items <- getLoan auth
+  -- TODO: handle error here
+  case eitherDecode @[JSONBook] (encode items) of
+    Right books -> do
+      mtime <- getCurrentTime
+      pure $ Right (mtime, coerce books)
+    Left err -> pure (Left $ Text.pack err)
